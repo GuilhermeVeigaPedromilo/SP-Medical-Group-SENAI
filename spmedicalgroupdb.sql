1b8e75076bd6cdc923d198e908af2075d4f8c443
Guilherme Veiga Pedromilo <guipedromilo@gmail.com>
2024-01-31 15:47:03 -0300

    database

--- spmedicalgroupdb.sql
+++ spmedicalgroupdb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Tempo de geração: 13-Dez-2023 às 11:01
--- Versão do servidor: 8.0.35-0ubuntu0.22.04.1
--- versão do PHP: 8.1.2-1ubuntu2.14
+-- Tempo de geração: 31/01/2024 às 15:46
+-- Versão do servidor: 8.0.36-0ubuntu0.22.04.1
+-- Versão do PHP: 8.1.2-1ubuntu2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `consultas`
+-- Estrutura para tabela `Blog`
+--
+
+CREATE TABLE `Blog` (
+  `id` int NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `postagem` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Despejando dados para a tabela `Blog`
+--
+
+INSERT INTO `Blog` (`id`, `nome`, `postagem`) VALUES
+(1, 'wwe', 'scsd'),
+(2, 'd cv', 'c xc'),
+(3, 'Pedromilo', 'Teste'),
+(4, 'Pedromilo', 'jadseihioes\\');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `consultas`
 --
 
 CREATE TABLE `consultas` (
@@ -38,7 +60,7 @@ CREATE TABLE `consultas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Extraindo dados da tabela `consultas`
+-- Despejando dados para a tabela `consultas`
 --
 
 INSERT INTO `consultas` (`id`, `username`, `email`, `date`, `horario`, `medico`, `informacoesamais`) VALUES
@@ -50,7 +72,7 @@ INSERT INTO `consultas` (`id`, `username`, `email`, `date`, `horario`, `medico`,
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `mensagens`
+-- Estrutura para tabela `mensagens`
 --
 
 CREATE TABLE `mensagens` (
@@ -65,7 +87,7 @@ CREATE TABLE `mensagens` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `users`
+-- Estrutura para tabela `users`
 --
 
 CREATE TABLE `users` (
@@ -80,7 +102,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Extraindo dados da tabela `users`
+-- Despejando dados para a tabela `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `cpf`, `sexo`, `datanascimento`, `email`, `tipo`) VALUES
@@ -96,33 +118,46 @@ INSERT INTO `users` (`id`, `username`, `password`, `cpf`, `sexo`, `datanasciment
 (40, 'Guilherme Ped', '7c222fb2927d828af22f592134e8932480637c0d', '000.000.000-00', 'Masculino', '2003-02-11', 'ped@gmail.com', 'user'),
 (46, 'A A', '356a192b7913b04c54574d18c28d46e6395428ab', '100.000.000-02', 'NaoInformado', '2023-12-13', 'a@ay.com', 'user'),
 (47, 'Joao', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', '222.222.222-22', 'Masculino', '2023-12-16', 'carolinelvr@gmail.com', 'user'),
-(48, 'Admin B', '7c222fb2927d828af22f592134e8932480637c0d', '111.111.111-11', 'Masculino', '1992-05-11', 'admin@gmail.com', 'Administrador');
+(48, 'Admin B', '7c222fb2927d828af22f592134e8932480637c0d', '111.111.111-11', 'Masculino', '1992-05-11', 'admin@gmail.com', 'Administrador'),
+(49, 'Pedromilo', '7c222fb2927d828af22f592134e8932480637c0d', '888.888.888-81', 'Masculino', '2007-05-12', 'pedromil@pedrovsk.com', 'user');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices para tabela `consultas`
+-- Índices de tabela `Blog`
+--
+ALTER TABLE `Blog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `consultas`
 --
 ALTER TABLE `consultas`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices para tabela `mensagens`
+-- Índices de tabela `mensagens`
 --
 ALTER TABLE `mensagens`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices para tabela `users`
+-- Índices de tabela `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT para tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `Blog`
+--
+ALTER TABLE `Blog`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `consultas`
@@ -140,7 +175,7 @@ ALTER TABLE `mensagens`
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
